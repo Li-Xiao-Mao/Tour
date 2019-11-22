e70610539a02e50cb9b2562be78e49436edd0ea0
linxiaomiao <282504669@qq.com>
2019-11-22 15:05:10 +0800

    项目开发准备已完成！

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+  import HomeHeader from '@/pages/home/components/HomeHeader'
+  export default {
+    name: "Home",
+    components: {
+      HomeHeader
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left"><i class="iconfont iconjiantou2"></i></div>
+    <div class="header-search">
+      <i class="iconfont iconfangdajing"></i>
+      请输入城市名称或关键字
+    </div>
+    <div class="header-right">广州</div>
+  </div>
+</template>
+
+<script>
+  import '@/assets/fonts/iconfont/iconfont.css'
+  export default {
+    name: "HomeHeader"
+  }
+</script>
+
+<style lang="less" scoped>
+  .header{
+    display:flex;
+    align-items: center;
+    height:86px;
+    line-height:86px;
+    background:#42B983;
+    .header-left{
+      width:64px;
+      text-align: center;
+      i{
+        font-size:45px
+      }
+    }
+    .header-search{
+      flex: 1;
+      height:64px;
+      line-height:64px;
+      color:#969896;
+      margin-left:20px;
+      background:#fff;
+      padding:0 10px;
+      border-radius:30px;
+      i{
+        font-size:35px
+      }
+    }
+    .header-right{
+      // width:140px;
+      margin:0 20px;
+      color:#fff;
+      text-align: center
+    }
+  }
+</style>
